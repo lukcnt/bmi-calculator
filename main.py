1063d2e70b46cb9d7789b20b44f9dadd7bc828aa
Lucas Cantoni <lukcnt@outlook.com>
2022-07-27 00:04:15 -0300

    String type variable converted to float type.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 #User input of height and weight.
 height = input("Enter your height in m: ")
 weight = input("Enter your weight in kg: ")
+
+#Convert string to float.
+height_converted = float(height)
+weight_converted = float(weight)
